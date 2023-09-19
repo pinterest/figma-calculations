b2128fb1f98ed794673ade52945fb00b8cf437c7
Dave Johnson <davejohnson@pinterest.com>
2023-09-19 10:57:17 -0700

    Added tests for checkFillStyleMatch() for bug fixing

---
+++ tests/__mocks__/figmaStyles.mock.ts
@@ -0,0 +1,205 @@
+import { FigmaTeamStyle } from "../../src/models/figma";
+
+export const FIGMA_STYLES: FigmaTeamStyle[] = [
+  {
+    "key": "3204b5a3bbe854cf4787cc2d8a4077a2074e6122",
+    "file_key": "cPXcu0NmmxRf5ywyybuxTU",
+    "node_id": "3449:597",
+    "style_type": "FILL",
+    "name": "Icon/Light-mode/$color-text-icon-brand-primary",
+    "description": "Used on text and icon solely to represent the Pinterest brand.\n\nApplies to both themes (light and dark mode).",
+    "created_at": "2022-06-01T20:04:42.126Z",
+    "updated_at": "2023-08-17T18:35:59.054Z",
+    "sort_position": " {n}}}",
+    "nodeDetails": {
+      "id": "3449:597",
+      "name": "Icon/Light-mode/$color-text-icon-brand-primary",
+      "type": "RECTANGLE",
+      "scrollBehavior": "SCROLLS",
+      "blendMode": "PASS_THROUGH",
+      "absoluteBoundingBox": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "absoluteRenderBounds": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "constraints": {
+        "vertical": "TOP",
+        "horizontal": "LEFT"
+      },
+      "fills": [
+        {
+          "blendMode": "NORMAL",
+          "type": "SOLID",
+          "color": {
+            "r": 0.9019607901573181,
+            "g": 0,
+            "b": 0.13725490868091583,
+            "a": 1
+          }
+        }
+      ],
+      "strokes": [],
+      "strokeWeight": 1,
+      "strokeAlign": "INSIDE",
+      "effects": []
+    }
+  },
+  {
+    "key": "24a30e6e9ea8800c51436cbfad083360867b73d8",
+    "file_key": "cPXcu0NmmxRf5ywyybuxTU",
+    "node_id": "7151:1418",
+    "style_type": "FILL",
+    "name": "Wash/opacity-400 (DarkGray 80%)",
+    "description": "-- Black 80%\n\nOverlay wash used on most surface's scrims. Used to supply a low-level of opacity over an element (e.g. Board cover overlay).\n",
+    "created_at": "2023-03-24T22:38:22.598Z",
+    "updated_at": "2023-04-05T00:24:41.020Z",
+    "sort_position": "~b",
+    "nodeDetails": {
+      "id": "7151:1418",
+      "name": "Wash/opacity-400 (DarkGray 80%)",
+      "type": "RECTANGLE",
+      "scrollBehavior": "SCROLLS",
+      "blendMode": "PASS_THROUGH",
+      "absoluteBoundingBox": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "absoluteRenderBounds": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "constraints": {
+        "vertical": "TOP",
+        "horizontal": "LEFT"
+      },
+      "fills": [
+        {
+          "opacity": 0.800000011920929,
+          "blendMode": "NORMAL",
+          "type": "SOLID",
+          "color": {
+            "r": 0.06666667014360428,
+            "g": 0.06666667014360428,
+            "b": 0.06666667014360428,
+            "a": 1
+          }
+        }
+      ],
+      "strokes": [],
+      "strokeWeight": 1,
+      "strokeAlign": "INSIDE",
+      "effects": []
+    }
+  },
+  {
+    "key": "c4a96a9c96abef825a408a83b792567b977c96fb",
+    "file_key": "cPXcu0NmmxRf5ywyybuxTU",
+    "node_id": "7081:1426",
+    "style_type": "FILL",
+    "name": "Extended color palette/$color-black-cosmicore-900",
+    "description": "Cosmicore black",
+    "created_at": "2023-03-24T22:38:22.572Z",
+    "updated_at": "2023-08-17T18:35:59.555Z",
+    "sort_position": "~_",
+    "nodeDetails": {
+      "id": "7081:1426",
+      "name": "Extended color palette/$color-black-cosmicore-900",
+      "type": "RECTANGLE",
+      "scrollBehavior": "SCROLLS",
+      "blendMode": "PASS_THROUGH",
+      "absoluteBoundingBox": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "absoluteRenderBounds": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "constraints": {
+        "vertical": "TOP",
+        "horizontal": "LEFT"
+      },
+      "fills": [
+        {
+          "blendMode": "NORMAL",
+          "type": "SOLID",
+          "color": {
+            "r": 0.06666667014360428,
+            "g": 0.06666667014360428,
+            "b": 0.06666667014360428,
+            "a": 1
+          }
+        }
+      ],
+      "strokes": [],
+      "strokeWeight": 1,
+      "strokeAlign": "INSIDE",
+      "effects": []
+    }
+  },
+  {
+    "key": "c72cd082ac28b7ac6f113c7a315440d79b094bb2",
+    "file_key": "cPXcu0NmmxRf5ywyybuxTU",
+    "node_id": "7081:1424",
+    "style_type": "FILL",
+    "name": "Extended color palette/$color-white-mochimalist-0",
+    "description": "Mochimalist white",
+    "created_at": "2023-03-24T22:38:22.455Z",
+    "updated_at": "2023-08-17T18:36:07.429Z",
+    "sort_position": "~]",
+    "nodeDetails": {
+      "id": "7081:1424",
+      "name": "Extended color palette/$color-white-mochimalist-0",
+      "type": "RECTANGLE",
+      "scrollBehavior": "SCROLLS",
+      "blendMode": "PASS_THROUGH",
+      "absoluteBoundingBox": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "absoluteRenderBounds": {
+        "x": 0,
+        "y": 0,
+        "width": 100,
+        "height": 100
+      },
+      "constraints": {
+        "vertical": "TOP",
+        "horizontal": "LEFT"
+      },
+      "fills": [
+        {
+          "blendMode": "NORMAL",
+          "type": "SOLID",
+          "color": {
+            "r": 1,
+            "g": 1,
+            "b": 1,
+            "a": 1
+          }
+        }
+      ],
+      "strokes": [],
+      "strokeWeight": 1,
+      "strokeAlign": "INSIDE",
+      "effects": []
+    }
+  },
+];
