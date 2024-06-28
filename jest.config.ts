c0cde5d9860d8ecf7a0d25ffb0cd59f02983b5af
Dave Johnson <davejohnson@pinterest.com>
2024-06-27 17:19:12 -0700

    Separated build and test ts configs so VS Code uses tsconfig for test files too so that typeRoots are used (but build will still exclude tests) Enable import path aliasing to start cleaning up imports relative paths

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,25 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
+import { type JestConfigWithTsJest, pathsToModuleNameMapper } from "ts-jest";
+import ts from "typescript";
+
+// Load the tsconfig.json file to get compilerOptions.baseUrl and compilerOptions.paths
+const configPath = ts.findConfigFile(
+  "./" /*searchPath*/,
+  ts.sys.fileExists,
+  "tsconfig.json"
+);
+if (!configPath) throw new Error("Could not find a valid 'tsconfig.json'.");
+
+const configFile = ts.readJsonConfigFile(configPath, ts.sys.readFile);
+const { compilerOptions } = ts.parseConfigFileTextToJson(
+  configPath,
+  configFile.getText()
+).config;
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: 'ts-jest',
-}
+  preset: "ts-jest",
+  roots: ["<rootDir>"],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+};
 
-export default jestConfig
+export default jestConfig;
